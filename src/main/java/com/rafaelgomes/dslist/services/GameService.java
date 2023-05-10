42475f10232495d639ca0d3ffd9115327182ef21
Rafael Gomes <rafaelcgomesneves@gmail.com>
2023-05-09 21:58:02 -0300

    Incluindo Service e Controller

---
+++ src/main/java/com/rafaelgomes/dslist/services/GameService.java
@@ -0,0 +1,26 @@
+package com.rafaelgomes.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafaelgomes.dslist.dto.GameMinDTO;
+import com.rafaelgomes.dslist.entities.Game;
+import com.rafaelgomes.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+	
+	
+	@Autowired
+	private GameRepository gameRepository;		
+			
+			
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+		
+	}
+}
